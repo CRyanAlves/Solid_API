a4fcfe4a67451f341c437d12d6aa542f58a860c8
Alex Manoel <alex.manoel@gestaokaizen.com.br>
2023-11-24 16:15:20 -0300

    first commit

---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,20 @@
+import { User } from "../../entities/User";
+import { IUserRepository } from "../../repositories/IUserRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
+export class CreateUseCase {
+  constructor(
+    private usersRepository: IUserRepository
+  ) {}
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExist = await this.usersRepository.findByEmail(data.email);
+
+    if(userAlreadyExist) {
+      throw new Error('User already exists');
+    }
+
+    const user = new User(data);
+
+    await this.usersRepository.save(user);
+  }
+}
